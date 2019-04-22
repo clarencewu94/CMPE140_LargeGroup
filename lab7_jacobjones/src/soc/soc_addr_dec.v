956982172749448ce7a3d8436e77712894bdbe7b
Jacob Jones <jacobjones@Jacobs-MacBook-Pro.local>
2019-04-22 12:35:49 -0700

    SoC decoder working

--- lab7_jacobjones/src/soc/soc_addr_dec.v
+++ lab7_jacobjones/src/soc/soc_addr_dec.v
@@ -7,7 +7,17 @@ module soc_addr_dec(
         output wire [1:0] RdSel
     );
 
-    
+    reg [4:0] ctrl;
+
+    assign {WE1, WE2, WEM, RdSel} = ctrl;
+
+    always @ (A) begin
+        case (A[7:4])
+            4'b0000: ctrl = 'b1_0_0_10;
+            4'b0001: ctrl = 'b0_1_0_11;
+            default: ctrl = 'b0_0_1_00;
+        endcase
+    end
 
 
 endmodule

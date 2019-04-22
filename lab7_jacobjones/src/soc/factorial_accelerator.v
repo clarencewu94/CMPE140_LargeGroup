fbe7ff78a69a6113458f8ae5375a5e41b133c5a6
Jacob Jones <jacobjones@Jacobs-MacBook-Pro.local>
2019-04-22 11:58:43 -0700

    Added Interface for GPIO and fact accel

---
+++ lab7_jacobjones/src/soc/factorial_accelerator.v
@@ -0,0 +1,13 @@
+module factorial_accelerator(
+        input wire clk,
+        input wire reset,
+        input wire [1:0] A,
+        input wire WE1,
+        input wire [3:0] WD,
+        output wire RD
+    );
+
+
+
+
+endmodule
---
+++ lab7_jacobjones/src/soc/soc_addr_dec.v
@@ -0,0 +1,13 @@
+module soc_addr_dec(
+        input wire WE,
+        input wire [31:0] A, // Check size
+        output wire WE1,
+        output wire WE2,
+        output wire WEM,
+        output wire [1:0] RdSel
+    );
+
+    
+
+
+endmodule

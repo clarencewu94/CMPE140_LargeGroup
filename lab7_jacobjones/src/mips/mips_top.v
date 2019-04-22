06234e6927ca41a913974dcd422bf3a25ece4dff
Jacob Jones <jacobjones@Jacobs-MacBook-Pro.local>
2019-04-22 11:58:43 -0700

    Added Interface for GPIO and fact accel

--- lab7_jacobjones/src/mips/mips_top.v
+++ lab7_jacobjones/src/mips/mips_top.v
@@ -14,6 +14,13 @@ module mips_top (
     );
 
     wire [31:0] DONT_USE;
+    wire WE1;
+    wire WE2;
+    wire WEM;
+    wire [1:0] RdSel;
+    wire [31:0] RdDM; 
+    wire [31:0] RdFA; 
+    wire [31:0] RdGPIO; 
 
     mips mips (
             .clk            (clk),
@@ -37,10 +44,46 @@ module mips_top (
 
     dmem dmem (
             .clk            (clk),
-            .we             (we_dm),
+            .we             (WEM),
             .a              (alu_out[7:2]),
             .d              (wd_dm),
-            .q              (rd_dm)
+            .q              (RdDM)
         );
+
+    gpio gpio (
+        .clk(clk),
+        .reset(rst),
+        .A(alu_out[3:2]),
+        .WE2(WE2),
+        .WD(wd_dm),
+        .RD(RdGPIO)
+    );
+
+    factorial_accelerator factorial_accelerator (
+        .clk(clk),
+        .reset(rst),
+        .A(alu_out[3:2]),
+        .WE1(WE1),
+        .WD(wd_dm[3:0]),
+        .RD(RdFA)
+    );
+
+    soc_addr_dec soc_addr_dec (
+        .WE(we_dm),
+        .A(alu_out),
+        .WE1(WE1),
+        .WE2(WE2),
+        .WEM(WEM),
+        .RdSel(RdSel)
+    );
+
+    mux4 #(32) read_data_mux(
+        .sel(RdSel),
+        .in0(RdDM),
+        .in1(RdDM),
+        .in2(RdFA),
+        .in3(RdGPIO),
+        .out(rd_dm)
+    );
 
 endmodule
---
+++ lab7_jacobjones/src/soc/gpio.v
@@ -0,0 +1,13 @@
+module gpio(
+        input wire clk,
+        input wire reset,
+        input wire [1:0] A,
+        input wire WE2,
+        input wire [31:0] WD,
+        output wire RD
+    );
+
+
+
+
+endmodule
